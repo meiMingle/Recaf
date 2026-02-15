b81c70aaee0eee172b94d5ac69b3f64f41d180b8
Tom Xin <766781886@qq.com>
2026-02-15 23:41:58 +0800

    update

--- recaf-ui/src/main/java/software/coley/recaf/ui/window/ScriptManagerWindow.java
+++ recaf-ui/src/main/java/software/coley/recaf/ui/window/ScriptManagerWindow.java
@@ -3,7 +3,10 @@ package software.coley.recaf.ui.window;
 import jakarta.enterprise.context.Dependent;
 import jakarta.inject.Inject;
 import javafx.scene.layout.BorderPane;
+import software.coley.recaf.Bootstrap;
+import software.coley.recaf.Recaf;
 import software.coley.recaf.services.window.WindowManager;
+import software.coley.recaf.ui.pane.LoggingPane;
 import software.coley.recaf.ui.pane.ScriptManagerPane;
 import software.coley.recaf.util.Lang;
 
@@ -15,14 +18,19 @@ import software.coley.recaf.util.Lang;
  */
 @Dependent
 public class ScriptManagerWindow extends AbstractIdentifiableStage {
+	private final Recaf recaf = Bootstrap.get();
 	@Inject
 	public ScriptManagerWindow(ScriptManagerPane scriptManagerPane) {
 		super(WindowManager.WIN_SCRIPTS);
 
+		LoggingPane logging = recaf.get(LoggingPane.class);
 		// Layout
 		titleProperty().bind(Lang.getBinding("menu.scripting.manage"));
 		setMinWidth(750);
 		setMinHeight(450);
-		setScene(new RecafScene(new BorderPane(scriptManagerPane), 750, 450));
+		BorderPane borderPane = new BorderPane(scriptManagerPane);
+		borderPane.setBottom(logging);
+		setScene(new RecafScene(borderPane, 750, 450));
+
 	}
 }
